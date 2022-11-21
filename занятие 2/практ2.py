f7658ee866081e7af1dfa35da86c6aaa35ad2b7b
ivan <ivansamorocovskii@gmail.com>
2022-11-21 16:35:47 +0300

    0

---
+++ занятие 2/практ2.py
@@ -0,0 +1,20 @@
+import math
+
+x = ((12.3*(10**(-1))))
+print("x =",x)
+
+y = ((15.4))
+print("y=", y)
+
+z = ((0.252*(10**3)))
+print("z=", z)
+
+s = (y**(x+1))/(((abs(y-2))**1/3)+3)+((x+y/2)/(2*abs(x+y)))*((x+1)**(-1/math.sin(z)))
+s1 = (y**(x+1))/(((abs(y-2))**1/3)+3)
+s2 = ((x+y/2)/(2*abs(x+y)))
+s3 = ((x+1)**(-1/math.sin(z)))
+print(s1,s2,s3,math.sin(z))
+
+print("s=",s)
+
+
